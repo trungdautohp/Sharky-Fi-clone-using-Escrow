ba741e6cc563d564517b29a408e1ec01b7d26a8a
trungdautohp <DeloachPriddy852@gmail.com>
2024-07-26 12:59:42 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("8ysKCbamzYNEoYWe6eosENVwTFmQq2eJNm4hRAgruwUF");
 
 #[program]
 pub mod nft_lend_borrow {
